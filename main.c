3484454d2af5310f11e29da0e892c94f236abb9d
DailyLucky <tangrui472@163.com>
2021-09-01 16:38:24 +0800

    Revert "新增一个关于#define的测试"
    This reverts commit 375c9f855f0dc3792acf46c7d7aff9fc8433b56d.

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-#define EMPTY
-#define SUM(x)  ((x) + (x))
 int main(void)
 {
-    int x = 10;
+    int a[5]={1,2,3,4,5};
+    int *ptr1=(int *)(&a+1); // 明白了。a可以理解为指向一维数组的指针，那么&a就是指向一位数组的指针的指针，
+                             // 所以&a+1 = &a(a的地址值) + 4(int类型占的字节数) * 5(a指向的数组的元素个数)
+    int *ptr2=(int *)((int)a+1);
 
-    printf("%d\n", SUM(x));
-    printf("SUM(x)");
+    printf("%x,%x",ptr1[-1],*ptr2);
     return 0;
 }
